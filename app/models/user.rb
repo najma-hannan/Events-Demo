d57a8d87ef060738c21332cdffc8bc3e769acdd9
sherry <sherry.kisilu@studebnt.moringaschool.com>
2023-05-24 14:40:47 +0300

    created schema for events

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,13 @@
 
 class User < ApplicationRecord
   has_secure_password
-
+  
   has_many :events, foreign_key: :organizer_id
   has_many :tickets, foreign_key: :user_id
 
   validates :username, presence: true, uniqueness: true
   validates :password, presence: true, length: { minimum: 6 }
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
-
+  validates :role, presence: true
   # Add any additional custom validations or methods as needed
 end
