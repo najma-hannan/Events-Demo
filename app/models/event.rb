c0c981c9acafc5fc96bf5622478f439da44d8314
jesse <jesse.kibaara@student.moringaschool.com>
2023-05-25 17:17:22 +0300

    event model code modified

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,6 @@
 class Event < ApplicationRecord
-  belongs_to :user
+  belongs_to :organizer, class_name: 'User'
+  has_many :tickets
+  has_many :event_registrations
+  has_many :event_feedbacks
 end
