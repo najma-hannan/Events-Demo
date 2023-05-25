7b7a73ce0ba12aefca111c37dd3a7ca26ef18f0d
Hannan <najma.mahat@student.moringaschool.com>
2023-05-26 02:25:55 +0300

    switch success code for login to 200

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:email])
     if user&.authenticate(params[:password])
       token = JwtService.encode_token(user_id: user.id)
-      render json: { user: user, token: token }, status: :created
+      render json: { user: user, token: token }, status: :ok
     else
       render json: { errors: ['Invalid email or password'] }, status: :unprocessable_entity
     end
