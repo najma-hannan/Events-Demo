768cd06b3c2aabb3ab446cc93d4cd1f4f23f2c7c
jesse <jesse.kibaara@student.moringaschool.com>
2023-05-26 00:03:18 +0300

    switch commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,20 @@
-
 class User < ApplicationRecord
   has_secure_password
-  
-  has_many :events, foreign_key: :organizer_id
-  has_many :tickets, foreign_key: :user_id
+
+  has_many :events, foreign_key: 'organizer_id'
 
   validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
   validates :password, presence: true, length: { minimum: 6 }
-  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
-  validates :role, presence: true
-  # Add any additional custom validations or methods as needed
+  validates :password_confirmation, presence: true
+
+  def self.authenticate(username, password)
+    user = find_by(username: username)
+    return user if user&.authenticate(password)
+  end
+
+  def admin?
+    role == 'admin'
+  end
 end
+
