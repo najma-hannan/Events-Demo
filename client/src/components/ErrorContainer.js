ff34e45178a2f0875ef8af62883fc396c5199fe9
Hannan <najma.mahat@student.moringaschool.com>
2023-05-29 00:46:44 +0300

    fix: check that errors is defined first

--- client/src/components/ErrorContainer.js
+++ client/src/components/ErrorContainer.js
@@ -1,7 +1,7 @@
 import { Alert } from "react-bootstrap";
 
 export default function ErrorContainer({ errors, className, clearErrors }) {
-    return errors.length > 0 ? <Alert className={className} variant="danger">
+    return errors?.length > 0 ? <Alert className={className} variant="danger">
         <ul className="">
             {errors.map((error, idx) => (
                 <li key={idx} className=''>{error}</li>
