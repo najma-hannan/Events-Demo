c68aa958b4e61e088af9346fe084e98b0b497969
Hannan <najma.mahat@student.moringaschool.com>
2023-05-29 01:53:56 +0300

    update event gallery ui

--- client/src/components/EventsGalleryPage.js
+++ client/src/components/EventsGalleryPage.js
@@ -1,6 +1,6 @@
-import { useLoaderData } from "react-router-dom";
+import { Link, useLoaderData } from "react-router-dom";
 import PageHeader from "./PageHeader";
-import { Alert, Container } from "react-bootstrap";
+import { Alert, Button, Card, Container } from "react-bootstrap";
 import axios from "axios";
 
 export async function loader() {
@@ -17,17 +17,29 @@ export default function EventsGalleryPage() {
     const events = useLoaderData();
 
     return <>
-        <PageHeader title="Events" />
+        <PageHeader title="Find your next hangout plan" />
 
         <section className="py-4">
-            <Container>
+            <Container fluid>
                 {
                     events.length > 0 ?
-                        events.map((event) => (
-                            <article key={event.id}>
-                                <p>{event.title}</p>
-                            </article>
-                        )) :
+                        <ul className="row row-cols-4 g-4">
+                            {events.map((event) => (
+                                <li key={event.id}>
+                                    <Card>
+                                        <Card.Img variant="top" src="" />
+                                        <Card.Body>
+                                            <Card.Title>{event.title}</Card.Title>
+                                            <Card.Text> <small className="text-muted"> Organized by: {event.organizer.name}</small></Card.Text>
+                                            <Card.Text>{event.description}</Card.Text>
+                                        </Card.Body>
+                                        <Card.Footer>
+                                            <Button size="sm" variant="link" as={Link} to={`/events/${event.id}`}>View Event</Button>
+                                        </Card.Footer>
+                                    </Card>
+                                </li>
+                            ))}
+                        </ul> :
                         <Alert variant="info" className="col-5 mx-auto">
                             <p>Uh-oh! Our archives seem to be empty at the moment. Come back later.</p>
                         </Alert>
