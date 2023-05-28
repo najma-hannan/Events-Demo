0f80c7466d73b22f854a10a1607b51891f61b2df
Hannan <najma.mahat@student.moringaschool.com>
2023-05-28 22:26:47 +0300

    ui: create reusable PageHeader
    - deps: install clsx lib for merging classNames

--- client/src/components/ProfilePage.js
+++ client/src/components/ProfilePage.js
@@ -3,6 +3,7 @@ import { useState } from "react";
 import { Button, Container, Form } from "react-bootstrap";
 import { useRevalidator, useRouteLoaderData } from "react-router-dom";
 import ErrorContainer from "./ErrorContainer";
+import PageHeader from "./PageHeader";
 
 export default function ProfilePage() {
     const revalidator = useRevalidator();
@@ -12,7 +13,7 @@ export default function ProfilePage() {
     const [isLoading, setIsLoading] = useState(false);
 
 
-    async function updateProfile (event) {
+    async function updateProfile(event) {
         event.preventDefault();
 
         setIsLoading(true);
@@ -37,30 +38,27 @@ export default function ProfilePage() {
     }
 
     return (
-        <Container className="py-4">
-            <h2 className="fs-3">Update Profile Details</h2>
+        <>
+            <PageHeader title="Update Profile Details" />
 
-            <ErrorContainer errors={errors} className={"container mt-1"} />
-
-            <section className="col-6">
-                <Form onSubmit={updateProfile}>
-                    <Form.Group className="mb-3">
-                        <Form.Label>Full Name</Form.Label>
-                        <Form.Control type="text" autoComplete='off' placeholder="Enter full name" name="name" defaultValue={user.name} required />
-                    </Form.Group>
-
-                    <Form.Group className="mb-3">
-                        <Form.Label>Email</Form.Label>
-                        <Form.Control type="text" autoComplete='off' placeholder="Enter full name" name="email" defaultValue={user.email} required />
-                    </Form.Group>
-
-                    <Button type="submit" disabled={isLoading} variant="primary">
-                        {isLoading ? "Saving..." : "Update Profile"}
-                    </Button>
-
-                </Form>
-            </section>
-
-        </Container>
+            <Container className="py-4">
+                <ErrorContainer errors={errors} className={"container mt-1"} />
+                <section className="col-6">
+                    <Form onSubmit={updateProfile}>
+                        <Form.Group className="mb-3">
+                            <Form.Label>Full Name</Form.Label>
+                            <Form.Control type="text" autoComplete='off' placeholder="Enter full name" name="name" defaultValue={user.name} required />
+                        </Form.Group>
+                        <Form.Group className="mb-3">
+                            <Form.Label>Email</Form.Label>
+                            <Form.Control type="text" autoComplete='off' placeholder="Enter full name" name="email" defaultValue={user.email} required />
+                        </Form.Group>
+                        <Button type="submit" disabled={isLoading} variant="primary">
+                            {isLoading ? "Saving..." : "Update Profile"}
+                        </Button>
+                    </Form>
+                </section>
+            </Container>
+        </>
     );
 }
