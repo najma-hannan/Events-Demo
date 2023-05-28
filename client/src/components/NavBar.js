d1793e03d07c8c38fe366a8319205e1b32fbfd63
Hannan <najma.mahat@student.moringaschool.com>
2023-05-28 14:51:16 +0300

    fix route definitions and update layout file

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link, useNavigate, useRouteLoaderData } from "react-router-dom";
 import { isAuthenticated, logout } from "../utils";
-import { Button, NavDropdown } from "react-bootstrap";
+import { Button } from "react-bootstrap";
 
 const Navbar = () => {
   const navigate = useNavigate();
@@ -17,25 +17,7 @@ const Navbar = () => {
       <div className="navbar-logo">
         <Link to="/">Events Hub</Link>
       </div>
-      <ul className="navbar-links">
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <NavDropdown title="Events" id="basic-nav-dropdown">
-            {/* Add your event links here */}
-            <NavDropdown.Item href="#event1">Event 1</NavDropdown.Item>
-            <NavDropdown.Item href="#event2">Event 2</NavDropdown.Item>
-            {/* ... */}
-          </NavDropdown>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/contact">Contact</Link>
-        </li>
-      </ul>
+
       {
         isAuthenticated() ?
         <div className="navbar-buttons">
