f8a89d73533a8b2c651db84b6be59629d7285a60
Hannan <najma.mahat@student.moringaschool.com>
2023-05-28 20:44:38 +0300

    switch start_date and end_date to datetime

--- db/migrate/20230525212550_events.rb
+++ db/migrate/20230525212550_events.rb
@@ -3,8 +3,8 @@ class Events < ActiveRecord::Migration[6.1]
     create_table :events do |t|
       t.string :title
       t.text :description
-      t.date :start_date
-      t.date :end_date
+      t.datetime :start_date
+      t.datetime :end_date
       t.string :location
       t.references :organizer, foreign_key: { to_table: :users }
 
