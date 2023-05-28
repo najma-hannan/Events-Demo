e89089899ea94c03fc8ed9e0cdcb37437a2cb4a6
Hannan <najma.mahat@student.moringaschool.com>
2023-05-28 22:38:55 +0300

    ui: update layout styles

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,15 +1,18 @@
 import React from 'react';
+import { Container, Stack } from 'react-bootstrap';
 
 const Footer = () => (
-    <footer>
-        <div>
-            <h4>About Us</h4>
-            <p>Events Hub, the best place to find your next event.</p>
-        </div>
-        <div>
-            <h4>Contact Us</h4>
-            <p>events@eventshub.com</p>
-        </div>
+    <footer className="bg-dark text-white">
+        <Stack direction='horizontal' gap={4} bg="dark" as={Container} className="d-flex py-5">
+            <div>
+                <h4 className="fw-bold text-info ">About Us</h4>
+                <p className="">Events Hub, the best place to find your next event.</p>
+            </div>
+            <div>
+                <h4 className="fw-bold text-info">Contact Us</h4>
+                <p>events@eventshub.com</p>
+            </div>
+        </Stack>
     </footer>
 );
 
--- client/src/components/ProfilePage.js
+++ client/src/components/ProfilePage.js
@@ -39,24 +39,27 @@ export default function ProfilePage() {
 
     return (
         <>
-            <PageHeader title="Update Profile Details" />
+            <PageHeader title="Update Profile" />
 
             <Container className="py-4">
                 <ErrorContainer errors={errors} className={"container mt-1"} />
-                <section className="col-6">
-                    <Form onSubmit={updateProfile}>
-                        <Form.Group className="mb-3">
-                            <Form.Label>Full Name</Form.Label>
-                            <Form.Control type="text" autoComplete='off' placeholder="Enter full name" name="name" defaultValue={user.name} required />
-                        </Form.Group>
-                        <Form.Group className="mb-3">
-                            <Form.Label>Email</Form.Label>
-                            <Form.Control type="text" autoComplete='off' placeholder="Enter full name" name="email" defaultValue={user.email} required />
-                        </Form.Group>
-                        <Button type="submit" disabled={isLoading} variant="primary">
-                            {isLoading ? "Saving..." : "Update Profile"}
-                        </Button>
-                    </Form>
+                <section className="card col-6">
+                    <div className="card-body">
+                        <h3 className="fs-4">Personal Details</h3>
+                        <Form className="mt-4" onSubmit={updateProfile}>
+                            <Form.Group>
+                                <Form.Label>Full Name</Form.Label>
+                                <Form.Control type="text" autoComplete='off' placeholder="Enter full name" name="name" defaultValue={user.name} required />
+                            </Form.Group>
+                            <Form.Group className="mt-3">
+                                <Form.Label>Email</Form.Label>
+                                <Form.Control type="text" autoComplete='off' placeholder="Enter full name" name="email" defaultValue={user.email} required />
+                            </Form.Group>
+                            <Button  className="mt-4" type="submit" disabled={isLoading} variant="primary">
+                                {isLoading ? "Saving..." : "Update Profile"}
+                            </Button>
+                        </Form>
+                    </div>
                 </section>
             </Container>
         </>
