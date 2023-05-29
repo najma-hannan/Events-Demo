6b7931e2f529b08faed3001e5af163388727f944
Hannan <najma.mahat@student.moringaschool.com>
2023-05-29 03:31:56 +0300

    wip(ui): render ticket information on singlevent page

--- client/src/components/SingleEventPage.js
+++ client/src/components/SingleEventPage.js
@@ -2,7 +2,7 @@ import React from 'react';
 import axios from 'axios';
 import { Link, useLoaderData } from 'react-router-dom';
 import PageHeader from './PageHeader';
-import { Alert, Button, Card, Col, Container, Row } from 'react-bootstrap';
+import { Alert, Button, Card, Col, Container, Form, Row, Stack } from 'react-bootstrap';
 import { dateFormatter } from '../utils';
 
 export async function loader({ params }) {
@@ -41,7 +41,7 @@ const SingleEventPage = () => {
       <PageHeader title={event.title} actions={<>
         <Button as={Link} variant="link" to={"/events"}>Back to event gallery</Button>
       </>} />
-      <Container className="mt-3" fluid="lg">
+      <Container className="mt-3 pb-5" fluid="lg">
         <Row className='gy-4'>
           <Col md={5}>
             <Card>
@@ -60,21 +60,29 @@ const SingleEventPage = () => {
             </Card>
           </Col>
 
-          <Col sm={12}>
+          <Col sm={12} md={7}>
             <Card>
               <Card.Header>Tickets</Card.Header>
               <Card.Body>
                 {
                   event?.tickets?.length > 0 ?
-                    event.tickets?.map((ticket) => (
-                      <div key={ticket.id}>
-                        <p>{ticket.name}</p>
-                        <input
-                          type="number"
-                          defaultValue="0"
-                        />
-                      </div>
-                    )) : <>
+                    <Form className="">
+                      <Stack gap={4}>
+                        {event.tickets?.map((ticket) => (
+                          <Form.Group key={ticket.id}>
+                            <Form.Label>{ticket.name}: <span className="fw-bold">KES {ticket.price}</span></Form.Label>
+                            <Form.Control
+                              type="number"
+                              defaultValue="0"
+                            />
+                          </Form.Group>
+                        ))}
+
+                        <div className="d-flex justify-content-end">
+                          <Button variant="success">Buy tickets</Button>
+                        </div>
+                      </Stack>
+                    </Form> : <>
                       <Alert variant="info">Ticket information is yet to be posted!</Alert>
                     </>}
               </Card.Body>
